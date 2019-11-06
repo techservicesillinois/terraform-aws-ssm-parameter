921ef867fbb86055b557f48f076f6264daa21456
Jon R. Roma <roma@illinois.edu>
2019-11-06 17:42:04 -0600

    Migrate to Terraform v0.12

--- main.tf
+++ main.tf
@@ -1,28 +1,24 @@
 resource "aws_ssm_parameter" "default" {
-  count       = "${length(var.parameters)}"
-  description = "${lookup(var.parameters[count.index], "description", "*** NO DESCRIPTION SET ***")}"
-  name        = "${format("%s/%s/%s", var.prefix, var.name, lookup(var.parameters[count.index], "name"))}"
-  type        = "${lookup(var.parameters[count.index], "type", "String")}"
-  value       = "${lookup(var.parameters[count.index], "value", "*** NO VALUE SET ***")}"
+  count = length(var.parameters)
+
+  description = lookup(
+    var.parameters[count.index],
+    "description",
+    "*** NO DESCRIPTION SET ***",
+  )
+
+  name = format(
+    "%s/%s/%s",
+    var.prefix,
+    var.name,
+    var.parameters[count.index]["name"],
+  )
+
+  type  = lookup(var.parameters[count.index], "type", "String")
+  value = lookup(var.parameters[count.index], "value", "*** NO VALUE SET ***")
 
   lifecycle {
     # Never update the value of an existing SSM parameter.
-    ignore_changes = [
-      "value",
-    ]
+    ignore_changes = [value]
   }
-}
-
-variable "parameters" {
-  description = "Parameters expressed as a list of maps, each entry of which may contain the following keys: description, name, type, and value."
-  default     = []
-}
-
-variable "name" {
-  description = "Parameter name"
-}
-
-variable "prefix" {
-  description = "Prefix prepended to parameter name if not using default"
-  default     = "/service"
 }
